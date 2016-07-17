a7d00599c1bdc9ff152447554d8f12ae4409aafb
Altruist2692 <altruist2692@gmail.com>
2016-07-17 10:34:08 +0530

    remoe condition

--- app/controllers/process_request_controller.rb
+++ app/controllers/process_request_controller.rb
@@ -1,9 +1,5 @@
 class ProcessRequestController < ApplicationController
 	def index
-		if params[‘hub.verify_token’] == “nishant”
-		 render text: params[‘hub.challenge’] and return
-		else
-		 render text: ‘error’ and return
-		end
+		 	render text: params[‘hub.challenge’] and return
 	end
 end
