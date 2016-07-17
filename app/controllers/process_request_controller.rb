56d328eede80fa3410378d5cd23fb7ca376c5e29
Altruist2692 <altruist2692@gmail.com>
2016-07-17 09:48:21 +0530

    add controller in action

---
+++ app/controllers/process_request_controller.rb
@@ -0,0 +1,2 @@
+class ProcessRequestController < ApplicationController
+end
---
+++ test/controllers/process_request_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ProcessRequestControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
