cc01698143b2d981adb104ba179fed658352b9d5
Altruist2692 <altruist2692@gmail.com>
2016-07-17 10:20:02 +0530

    fix issue with format

--- app/controllers/process_request_controller.rb
+++ app/controllers/process_request_controller.rb
@@ -1,9 +1,6 @@
 class ProcessRequestController < ApplicationController
 	def index
 		logger.debug "PARAMS => >>>>> #{params}"
-		hub_challenge = params[:hub_challenge]
-		respond_to do |format|
-			format.all {render text: 	hub_challenge}
-		end
+		@hub_challenge = params[:hub_challenge]
 	end
 end
