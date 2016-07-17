40536ee7d04b4372d5c398e81c45e1f8499cacac
Altruist2692 <altruist2692@gmail.com>
2016-07-17 10:00:41 +0530

    set hub challenge

--- app/controllers/process_request_controller.rb
+++ app/controllers/process_request_controller.rb
@@ -1,2 +1,7 @@
 class ProcessRequestController < ApplicationController
+	def index
+		logger.debug "PARAMS => >>>>> #{params}"
+		hub_challenge = params[:hub_challenge]
+		render text: 	hub_challenge
+	end
 end
